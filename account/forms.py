2bc3c12eabc82a07268a50ce94543ea3c53f429e
Levit Osei Kanner <lkanner21@gmail.com>
2020-11-06 19:28:09 +0000

    Creates forms for user model and profile model to help edit user data

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import Profile
 
 # class LoginForm(forms.Form):
 #     username = forms.CharField(max_length=20)
@@ -19,4 +20,12 @@ class UserRegistrationForm(forms.ModelForm):
         return data['password2']
             
         
-    
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('username', 'last_name', 'email')
+        
+class ProfileEditForm(forms.ModelForm):
+    class Meta: 
+        model =Profile
+        fields = ('date_of_birth', 'photo')
